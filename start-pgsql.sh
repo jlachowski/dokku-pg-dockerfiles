5f9489cfd1782500409bed13ace703aeda0e989d
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2014-03-25 16:30:39 +0100

    chown added

--- start-pgsql.sh
+++ start-pgsql.sh
@@ -8,4 +8,5 @@ if [[ ! -f /opt/postgresql/initialized ]]; then
     su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres --single  -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf" <<< "CREATE DATABASE db;"
     touch /opt/postgresql/initialized
 fi
+chown -R postgres:postgres /opt/postgresql
 su postgres sh -c "/usr/lib/postgresql/9.1/bin/postgres           -D  /var/lib/postgresql/9.1/main  -c config_file=/etc/postgresql/9.1/main/postgresql.conf  -c listen_addresses=*"
