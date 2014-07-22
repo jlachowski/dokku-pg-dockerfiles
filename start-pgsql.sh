628a259a1b53877b63772ae933bdccf122a5a38d
jlachowski <jalachowski@gmail.com>
2014-07-22 14:47:30 +0200

    owner & template added

--- start-pgsql.sh
+++ start-pgsql.sh
@@ -5,7 +5,7 @@ if [[ ! -f /opt/postgresql/initialized ]]; then
     cp -a /var/lib/postgresql/* /opt/postgresql/
     chown -R postgres:postgres /opt/postgresql
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
-    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db WITH ENCODING='UTF8';"
+    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db WITH ENCODING 'UTF8' OWNER root TEMPLATE template0 ;"
     touch /opt/postgresql/initialized
 fi
 chown -R postgres:postgres /opt/postgresql
